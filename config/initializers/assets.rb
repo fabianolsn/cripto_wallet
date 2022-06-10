e822ba7229c42328033fe5c73938b4e5f29d9aac
fabianolsn <fabiano.lima.pro@gmail.com>
2022-06-09 22:22:11 -0300

    teste de mudans

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,18 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w(
+                                                articles.js
+                                                coins.js
+                                                mining_type.js
+                                                welcome.js
+                                                )
+                                                
+Rails.application.config.assets.precompile += %w(
+                                                articles.css
+                                                coins.css
+                                                mining_type.css
+                                                scaffolds.css
+                                                welcome.css
+                                                )
+
